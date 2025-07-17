7d80088b47b6ea9774fb5d2127f293c6584bcc70
Lucca Duarte Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2025-07-17 19:25:35 -0400

    created minimalist SPI peripheral behavioral model

--- tests/spi/spi_top_test.sv
+++ tests/spi/spi_top_test.sv
@@ -40,27 +40,104 @@ module spi_top_tb;
   // 
 
   // TODO: DRDY_L controlled by number of bytes shifted out + delay
-
-  logic [7:0] MISO_data = 8'hA5;
-  logic [2:0] bit_index = 3'd7;  // Start with MSB
+  logic [7:0] MISO_data = 8'h00;
+  logic [7:0] MOSI_data = 8'h00;
+  logic [2:0] dout_bit_index = 4'd7;  // Start with MSB
+  logic [2:0] din_bit_index = 4'd7;  // Start with MSB  
+  transaction_t received_cmd = CMD_NONE;
+  logic [1:0] RDATA_index = 2'd2;
+  
+  logic [23:0] conversion_data = 24'h12_34_56;
 
   // Set up data on posedge of SCLK (well before negedge, when 
   // it's actually shifted into the SPI Master)
   always @(posedge SCLK) begin
-      if (~CS_L) begin            // Only shift when chip is selected
-          MISO <= MISO_data[bit_index];
-          if (bit_index > 0)
-              bit_index <= bit_index - 1;
-          else
-              bit_index <= 3'd7;  // Restart or hold value
-      end else begin
-          bit_index <= 3'd7;      // Reset if chip deselected
-          MISO <= 1'bz;           // Tri-state MISO when not selected
-      end
+    if (~CS_L) begin                    // Only shift when chip is selected
+      case (received_cmd)
+
+        // Determine which command was just received
+        CMD_NONE: begin
+          case (MOSI_data)
+          8'hF0:
+            received_cmd <= SELFCAL;
+          8'h01: begin
+            received_cmd <= RDATA;
+            RDATA_index = 2'd2;
+          end
+          8'h03: begin
+            received_cmd <= RDATAC;
+            RDATA_index = 2'd2;
+          end
+          8'h0F:
+            received_cmd <= SDATAC;
+          8'h1?:
+            received_cmd <= RREG;
+          8'h5?:
+            received_cmd <= WREG;          
+          default: ;
+        endcase
+        end
+
+        // Single-shot conversion
+        RDATA: begin
+          // 1st SCLK rising edge
+          if (din_bit_index == 4'd7) begin
+            if (RDATA_index >= 0) begin
+              RDATA_index = RDATA_index - 4'd1;
+            end else begin
+              RDATA_index = 4'd2;
+              received_cmd <= CMD_NONE;
+            end
+          end
+
+          MISO_data = conversion_data[8 * RDATA_index +: 8];
+        end
+
+        // Continuous conversions
+        RDATAC: begin
+          // 1st SCLK rising edge
+          if (din_bit_index == 4'd7) begin
+            if (RDATA_index > 0) begin
+              RDATA_index = RDATA_index - 4'd1;
+            end else begin
+              RDATA_index = 4'd2;
+            end
+          end
+
+          MISO_data = conversion_data[8 * RDATA_index +: 8];
+        end
+
+        default: ;
+      endcase
+      
+      MISO <= MISO_data[dout_bit_index];   // shift out
+      
+      if (dout_bit_index > 0) 
+        dout_bit_index <= dout_bit_index - 1;
+      else
+        dout_bit_index <= 3'd7;            // Restart or hold value
+    end
+  end  
+
+  always @(negedge SCLK) begin
+    if (~CS_L) begin
+      MOSI_data[din_bit_index] <= MOSI;   // shift in
+
+      if (din_bit_index > 0)
+        din_bit_index <= din_bit_index - 1;
+      else                
+        din_bit_index <= 3'd7;
+    end
   end
 
-  always @(posedge dut.spi_master.done_o) begin
-    MISO_data += 1;
+  // reset parameters when chip is de-selected
+  always @(posedge CS_L) begin
+    MISO_data = 8'h00;
+    MOSI_data = 8'h00;
+    received_cmd = CMD_NONE;
+    dout_bit_index = 3'd7;
+    din_bit_index = 3'd7;
+    MISO = 1'bz;    
   end
 
   // Task to run one transaction
@@ -103,6 +180,7 @@ module spi_top_tb;
     // TODO: sweep thru multiple commands to test this thoroughly
 
     $display("Starting RDATA transaction...");
+
     fork
       begin
         #1000 pulse_DRDY();
@@ -111,23 +189,23 @@ module spi_top_tb;
         #33333 pulse_DRDY();
       end
       begin
-        run_transaction(SELFCAL);
+        run_transaction(RDATA);
       end
     join
 
-    // $display("Starting RDATAC transaction...");
-    // fork
-    //   begin
-    //     #1000 pulse_DRDY();
-    //     #3000 pulse_DRDY();
-    //     #3000 pulse_DRDY();
-    //     #3000 pulse_DRDY();
-    //     #3000 pulse_DRDY(); // needed for SDATAC condition
-    //   end
-    //   begin
-    //     run_transaction(RDATAC);
-    //   end
-    // join
+    $display("Starting RDATAC transaction...");
+    fork
+      begin
+        #1000 pulse_DRDY();
+        #3000 pulse_DRDY();
+        #3000 pulse_DRDY();
+        #3000 pulse_DRDY();
+        #3000 pulse_DRDY(); // needed for SDATAC condition
+      end
+      begin
+        run_transaction(RDATAC);
+      end
+    join
 
     $display("Test complete.");
     $finish;
