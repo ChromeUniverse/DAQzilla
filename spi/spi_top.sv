ccc928b5f0be90160f10e431ed9e0e5424e3b641
Lucca Duarte Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2025-07-21 13:12:24 -0400

    updated SPI top + testbench

--- spi/spi_top.sv
+++ spi/spi_top.sv
@@ -1,24 +1,24 @@
 `default_nettype none
 
 module spi_top (
-  input wire clock_i, reset_i,
-  input wire DRDY_L_i,
+  input logic clock_i, reset_i,
+  input logic DRDY_L_i,
 
   // Transaction handler
-  input wire transaction_start_i,
-  input transaction_t transaction_i,
-  output wire transaction_done_o,
+  input logic transaction_start_i,
+  input [23:0] cmd_i,
+  output logic transaction_done_o,
+
+  // Conversion data
+  output logic [23:0] data_o,
 
   // SPI interface
-  input wire MISO_i,
-  output wire MOSI_o, SCLK_o, auto_CS_o, CS_L_o
+  input logic MISO_i,
+  output logic MOSI_o, SCLK_o, auto_CS_o, CS_L_o
 );
 
-  // shared lines
-  wire spi_start, spi_done;
-  wire [7:0] spi_tx_buffer, spi_rx_buffer;
-
-  // TODO: SPI master
+  logic spi_start, spi_done;
+  logic [7:0] spi_tx_buffer, spi_rx_buffer;
 
   spi #(.WIDTH(8)) spi_master (
     .reset_i(reset_i),
@@ -37,36 +37,48 @@ module spi_top (
     .auto_CS_o(auto_CS_o)    // asserted only while a transfer is happening
   );
 
-  // TODO: SPI transaction layer
+  logic [1:0] reg_read_sel;
+  logic reg_read_load;
 
   spi_transaction_layer handler (
-  .clock_i(clock_i),
-  .reset_i(reset_i),
+    .clock_i(clock_i),
+    .reset_i(reset_i),
 
-  // Transaction control interface
-  .start_i(transaction_start_i),
-  .done_o(transaction_done_o),
-  .transaction_i(transaction_i),
-  .reg_addr_i(),
+    // Transaction control interface
+    .start_i(transaction_start_i),
+    .done_o(transaction_done_o),
+    .cmd_i(cmd_i),
 
-  // Register file control interface
-  .register_file_sel_in_o(),
-  .register_file_load_o(),
+    // Register file control interface
+    .read_reg_sel(reg_read_sel),
+    .read_reg_load(reg_read_load),
 
-  // ADS1256 interrupts
-  .DRDY_L_i(DRDY_L_i),
-  
-  // SPI Master interface
-  .spi_start_o(spi_start),
-  .spi_done_i(spi_done),
-  .CS_L_o(CS_L_o),
-  .tx_buffer_o(spi_tx_buffer)
+    // ADS1256 interrupts
+    .DRDY_L_i(DRDY_L_i),
+    
+    // SPI Master interface
+    .spi_start_o(spi_start),
+    .spi_done_i(spi_done),
+    .CS_L_o(CS_L_o),
+    .tx_buffer_o(spi_tx_buffer)
+  );
+
+  logic [7:0] reg_file_RREG;
+  logic [23:0] reg_file_conversion_data;
+
+  register_file #(
+    .REG_COUNT(4),
+    .REG_WIDTH(8)
+  ) reg_file (
+    .clock_i(clock_i),
+    .sel_in_i(reg_read_sel),
+    .load_i(reg_read_load),
+    .clear_i(reset_i),
+    .in_i(spi_rx_buffer),
+    .RREG_out_o(reg_file_RREG),
+    .conversion_data_o(reg_file_conversion_data)
+  );
 
   // TODO: ADS1256 system controller
-
-  // TODO: register file
-
-  // TODO: databus interconnect
-);
   
 endmodule
--- tests/spi/spi_top_test.sv
+++ tests/spi/spi_top_test.sv
@@ -14,11 +14,23 @@ module spi_top_tb;
 
   // Transaction interface
   logic transaction_start;
-  transaction_t transaction;
   logic transaction_done;
+  logic [23:0] command;
+
+  // Commands
+  localparam CMD_NONE_BINARY = 24'h00_00_00;
+  localparam CMD_WAKEUP   = 24'hFF_FF_FF;
+  localparam CMD_RDATA    = 24'h01_FF_FF;
+  localparam CMD_RDATAC   = 24'h03_FF_FF;
+  localparam CMD_SDATAC   = 24'h0F_FF_FF;
+  localparam CMD_SELFCAL  = 24'hF0_FF_FF;
+  localparam CMD_RREG     = 24'h17_00_69;
+  localparam CMD_WREG     = 24'h57_00_69;
 
   // DRDY_L from ADC
   logic DRDY_L;
+
+  logic [23:0] conversion_data_out;
 
   // DUT instantiation
   spi_top dut (
@@ -26,13 +38,14 @@ module spi_top_tb;
     .reset_i(reset),
     .DRDY_L_i(DRDY_L),
     .transaction_start_i(transaction_start),
-    .transaction_i(transaction),
+    .cmd_i(command),
     .transaction_done_o(transaction_done),
     .MISO_i(MISO),
     .MOSI_o(MOSI),
     .SCLK_o(SCLK),
     .auto_CS_o(auto_CS),
-    .CS_L_o(CS_L)
+    .CS_L_o(CS_L),
+    .data_o(conversion_data_out)
   );
 
   // 
@@ -40,11 +53,13 @@ module spi_top_tb;
   // 
 
   // TODO: DRDY_L controlled by number of bytes shifted out + delay
+  // TODO: need logic for RREG and WREG commands
   logic [7:0] MISO_data = 8'h00;
   logic [7:0] MOSI_data = 8'h00;
   logic [2:0] dout_bit_index = 4'd7;  // Start with MSB
   logic [2:0] din_bit_index = 4'd7;  // Start with MSB  
-  transaction_t received_cmd = CMD_NONE;
+  
+  transaction_t received_cmd = TRANSACTION_NONE;
   logic [1:0] RDATA_index = 2'd2;
   
   logic [23:0] conversion_data = 24'h12_34_56;
@@ -56,26 +71,23 @@ module spi_top_tb;
       case (received_cmd)
 
         // Determine which command was just received
-        CMD_NONE: begin
-          case (MOSI_data)
-          8'hF0:
-            received_cmd <= SELFCAL;
-          8'h01: begin
-            received_cmd <= RDATA;
-            RDATA_index = 2'd2;
-          end
-          8'h03: begin
-            received_cmd <= RDATAC;
-            RDATA_index = 2'd2;
-          end
-          8'h0F:
-            received_cmd <= SDATAC;
-          8'h1?:
-            received_cmd <= RREG;
-          8'h5?:
-            received_cmd <= WREG;          
-          default: ;
-        endcase
+        TRANSACTION_NONE: begin
+          casez (MOSI_data)
+            8'hF0:
+              received_cmd <= SELFCAL;
+            8'h01: begin
+              received_cmd <= RDATA;
+              RDATA_index = 2'd2;
+            end
+            8'h03: begin
+              received_cmd <= RDATAC;
+              RDATA_index = 2'd2;
+            end
+            8'h0F:        received_cmd <= SDATAC;
+            8'b0001_????: received_cmd <= RREG;
+            8'b0101_????: received_cmd <= WREG;
+            default: ;
+          endcase
         end
 
         // Single-shot conversion
@@ -86,7 +98,7 @@ module spi_top_tb;
               RDATA_index = RDATA_index - 4'd1;
             end else begin
               RDATA_index = 4'd2;
-              received_cmd <= CMD_NONE;
+              received_cmd <= TRANSACTION_NONE;
             end
           end
 
@@ -94,18 +106,32 @@ module spi_top_tb;
         end
 
         // Continuous conversions
-        RDATAC: begin
-          // 1st SCLK rising edge
-          if (din_bit_index == 4'd7) begin
-            if (RDATA_index > 0) begin
-              RDATA_index = RDATA_index - 4'd1;
-            end else begin
-              RDATA_index = 4'd2;
-            end
+        RDATAC: begin          
+          // SDATAC issued: stop conversions
+          // "The command must be issued after DRDY goes low and completed before DRDY goes high." (p.35)
+          if (~DRDY_L & MOSI_data == 8'h0F & din_bit_index == 4'd7) begin
+            RDATA_index = 4'd2;
+            received_cmd <= TRANSACTION_NONE;
           end
 
-          MISO_data = conversion_data[8 * RDATA_index +: 8];
+          else begin
+            // 1st SCLK rising edge
+            if (din_bit_index == 4'd7) begin
+              if (RDATA_index > 0) begin
+                RDATA_index = RDATA_index - 4'd1;
+              end else begin
+                RDATA_index = 4'd2;
+              end
+            end
+
+            MISO_data = conversion_data[8 * RDATA_index +: 8];
+            
+          end          
         end
+
+        // TODO: RREG
+        // TODO: WREG
+        // TODO: SELFCAL
 
         default: ;
       endcase
@@ -134,17 +160,17 @@ module spi_top_tb;
   always @(posedge CS_L) begin
     MISO_data = 8'h00;
     MOSI_data = 8'h00;
-    received_cmd = CMD_NONE;
+    received_cmd = TRANSACTION_NONE;
     dout_bit_index = 3'd7;
     din_bit_index = 3'd7;
     MISO = 1'bz;    
   end
 
   // Task to run one transaction
-  task run_transaction(transaction_t cmd);
+  task run_transaction(logic [23:0] cmd);
     begin
       @(negedge clock);
-      transaction = cmd;
+      command = cmd;
       transaction_start = 1;
       @(negedge clock);
       transaction_start = 0;
@@ -170,17 +196,16 @@ module spi_top_tb;
     // Initialize
     reset = 1;
     transaction_start = 0;
-    transaction = RDATAC;
     MISO = 1'b0;
     DRDY_L = 1;
     @(negedge clock);
     reset = 0;
 
-    // TODO: handle SDATAC for RDATAC
     // TODO: sweep thru multiple commands to test this thoroughly
 
+    // RDATA
+
     $display("Starting RDATA transaction...");
-
     fork
       begin
         #1000 pulse_DRDY();
@@ -189,23 +214,70 @@ module spi_top_tb;
         #33333 pulse_DRDY();
       end
       begin
-        run_transaction(RDATA);
+        run_transaction(CMD_RDATA);
       end
     join
 
-    $display("Starting RDATAC transaction...");
-    fork
-      begin
-        #1000 pulse_DRDY();
-        #3000 pulse_DRDY();
-        #3000 pulse_DRDY();
-        #3000 pulse_DRDY();
-        #3000 pulse_DRDY(); // needed for SDATAC condition
-      end
-      begin
-        run_transaction(RDATAC);
-      end
-    join
+    // RDATAC
+
+    // $display("Starting RDATAC transaction...");
+    // fork
+    //   begin
+    //     #1000 pulse_DRDY();
+    //     #3000 pulse_DRDY();
+    //     #3000 pulse_DRDY();
+    //     #3000 pulse_DRDY();
+    //     #3000 pulse_DRDY(); // needed for SDATAC condition
+    //   end
+    //   begin
+    //     // mannually run RDATAC transation
+    //     @(negedge clock);
+    //     command = CMD_RDATAC;
+    //     transaction_start = 1;
+    //     @(negedge clock);
+    //     transaction_start = 0;
+
+    //     // Wait for 3 complete conversions
+
+    //     wait (dut.handler.FSM.state == dut.handler.FSM.RDATAC_WAIT_DRDY_C);
+    //     $display("1st conversion complete");
+    //     repeat (2) @(posedge clock);
+    //     wait (dut.handler.FSM.state == dut.handler.FSM.RDATAC_WAIT_DRDY_C);
+    //     $display("2nd conversion complete");
+    //     repeat (2) @(posedge clock);
+    //     wait (dut.handler.FSM.state == dut.handler.FSM.RDATAC_WAIT_DRDY_C);
+    //     $display("3rd conversion complete");
+
+    //     repeat (2) @(posedge clock);
+
+    //     // issue SDATAC command
+    //     command = CMD_SDATAC;
+    //     wait (transaction_done);
+
+    //     repeat (100) @(posedge clock);
+    //   end
+    // join
+
+    // SELFCAL
+
+    // $display("Starting RREG transaction...");
+    // fork
+    //   run_transaction(CMD_SELFCAL);
+    // join
+
+    // RREG
+
+    // $display("Starting RREG transaction...");
+    // fork
+    //   run_transaction(CMD_RREG);
+    // join
+
+    // WREG
+
+    // $display("Starting WREG transaction...");
+    // fork
+    //   run_transaction(CMD_WREG);
+    // join
 
     $display("Test complete.");
     $finish;
